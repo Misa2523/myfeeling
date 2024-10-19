3ec55899063aeaecd6fb70de4d23259a0d21bb56
Misa2523 <sa5chsgrnccon2510@gmail.com>
2024-10-19 07:21:39 +0000

    [Add]ユーザー側の全コントローラー作成

---
+++ app/controllers/public/commentes_controller.rb
@@ -0,0 +1,10 @@
+class Public::CommentesController < ApplicationController
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,41 @@ Rails.application.routes.draw do
     sessions: "public/sessions"
   }
 
+  #ユーザー側のルーティング設定
+  namespace :public do
+    get 'relationships/create'
+    get 'relationships/destroy'
+
+    get 'favorites/create'
+    get 'favorites/destroy'
+    get 'favorites/index'
+
+    get 'commentes/create'
+    get 'commentes/update'
+    get 'commentes/destroy'
+
+    get 'feeling_posts/new'
+    get 'feeling_posts/create'
+    get 'feeling_posts/index'
+    get 'feeling_posts/show'
+    get 'feeling_posts/edit'
+    get 'feeling_posts/update'
+    get 'feeling_posts/destroy'
+    get 'feeling_posts/search'
+
+    get 'users/index'
+    get 'users/show'
+    get 'users/edit'
+    get 'users/update'
+    get 'users/search'
+    get 'users/check'
+    get 'users/out'
+
+    get 'homes/top'
+    get 'homes/about'
+  end
+
+
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
