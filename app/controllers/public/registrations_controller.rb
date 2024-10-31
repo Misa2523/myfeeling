07d0b276ccc2e9b06d5e5df0068b3439ae98432d
Misa2523 <sa5chsgrnccon2510@gmail.com>
2024-10-31 12:03:52 +0000

    [Add]deviseを使ったユーザー登録時に追加のカスタム属性を許可する設定

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Public::RegistrationsController < Devise::RegistrationsController
+
+  before_action :configure_permitted_parameters, if: :devise_controller? #deviseコントローラーに対しリクエストが行われている時に実行
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
@@ -38,7 +40,15 @@ class Public::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
-  # protected
+  protected
+
+  #deviseを使ったユーザー登録時に、追加のカスタム属性を許可するメソッド
+  def configure_permitted_parameters
+    #devise_parameter_sanitizer：Deviseによって提供されるパラメータを安全に処理する
+    #psermit：指定したアクションで受け入れることができるパラメータを定義
+    #keys:[...]：許可するパラメータのリストを定義
+    devise_parameter_sanitizer.permit(:sign_up, keys:[:name, :name_kana, :telephone_number, :birthday])
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_up_params
