02f435852a2883b7ce23022ce0fc82576fbd8c71
Misa2523 <sa5chsgrnccon2510@gmail.com>
2024-10-31 12:14:26 +0000

    [Add]ログイン/ログアウト/新規登録した際の遷移先の設定

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -6,6 +6,10 @@ class Public::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
+  def after_sign_up_path_for(resource) #resourceには新規登録したユーザーの情報が渡される
+    user_path #リダイレクト先を設定
+  end
+
   # GET /resource/sign_up
   # def new
   #   super
