e92e7d1b0a2b094030c435b64930b439a886dca4
Misa2523 <sa5chsgrnccon2510@gmail.com>
2024-11-06 09:56:04 +0000

    [Add]投稿に対しいいねがされたかどうか判断するメソッドの追記

--- app/models/feeling_post.rb
+++ app/models/feeling_post.rb
@@ -9,4 +9,9 @@ class FeelingPost < ApplicationRecord
   validates :title, presence: true
   validates :body, presence: true
 
+  #引数で渡されたユーザidがFavoriteテーブル内に存在(exists?)するかどうかを調べる
+  def favorited_by?(user)
+    favorites.exists?(user_id: user.id)
+  end
+
 end
