9650b5acafb8f7922d04612311bd71a6e0114324
Misa2523 <sa5chsgrnccon2510@gmail.com>
2024-11-06 09:24:10 +0000

    [Add]comment/favoriteモデルのアソシエーション/バリデーション設定

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,2 +1,13 @@
 class Favorite < ApplicationRecord
+
+  #アソシエーション
+  belongs_to :user
+  belongs_to :feeling_post
+
+  #バリデーション
+  #同じ人が同じ投稿に複数回いいねしないよう制限
+  validates :user_id, uniqueness: {scope: :feeling_post_id}
+  #uniqueness：validatesメソッドの引数user_idカラムの値がすでにテーブルに保存されている値と重複してないかをチェック
+  #scope：user_idとfeeling_post_idのペアに対し、既に同じ値のペアがテーブルに保存されてないか判定
+
 end
--- app/models/feeling_post.rb
+++ app/models/feeling_post.rb
@@ -2,6 +2,8 @@ class FeelingPost < ApplicationRecord
 
   #アソシエーション
   belongs_to :user
+  has_many :favorites, dependent: :destroy
+  has_many :comments, dependent: :destroy
 
   #バリデーション
   validates :title, presence: true
