1cf08196cbd27ea1b3226b31f71b0a32bfcb4c6d
Misa2523 <sa5chsgrnccon2510@gmail.com>
2024-11-09 11:54:41 +0000

    [Add]管理者データ登録

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -4,7 +4,7 @@ class Public::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
 
   def after_sign_in_path_for(resource) #resourceにはサインインしたユーザーの情報が渡される
-    root_path #リダイレクト先を設定
+    user_path #リダイレクト先を設定
   end
 
   def after_sign_out_path_for(resource_or_scope) #resource_or_scopeにはサインアウトしたリソースやスコープの情報が渡される
