cda519ce16f94b7129f3dc16f3b588fdeb81fce6
Misa2523 <sa5chsgrnccon2510@gmail.com>
2024-11-15 10:10:07 +0000

    [Add]不要なコメント削除

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -1,7 +1,4 @@
-# frozen_string_literal: true
-
 class Public::SessionsController < Devise::SessionsController
-  # before_action :configure_sign_in_params, only: [:create]
 
   def after_sign_in_path_for(resource) #resourceにはサインインしたユーザーの情報が渡される
     user_path(current_user.id) #リダイレクト先を設定
@@ -11,25 +8,6 @@ class Public::SessionsController < Devise::SessionsController
     about_path #アバウトページへ遷移
   end
 
-  # GET /resource/sign_in
-  # def new
-  #   super
-  # end
+  protected
 
-  # POST /resource/sign_in
-  # def create
-  #   super
-  # end
-
-  # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
-
-  # protected
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
 end
