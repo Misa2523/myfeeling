e917b6b4018ba8d8b0005e2c88b94f446a2113ad
Misa2523 <sa5chsgrnccon2510@gmail.com>
2024-11-22 05:43:51 +0000

    [Add]トップページ/新規投稿ページ作成

--- app/controllers/public/feeling_posts_controller.rb
+++ app/controllers/public/feeling_posts_controller.rb
@@ -1,8 +1,20 @@
 class Public::FeelingPostsController < ApplicationController
   def new
+    # viewへ渡すためのインスタンス変数に空のmodelオブジェクトを生成
+    @feeling_post = FeelingPost.new
   end
 
   def create
+    # データを受け取り新規登録するためのインスタンス作成
+    @feeling_post = FeelingPost.new(feeling_post_params)
+    # データをデータベースに保存するためのsaveメソッド実行
+    if @feeling_post.save
+      flash[:notice] = "投稿しました"
+      redirect_to feeling_post_path(@feeling_post)
+    else
+      flash.now[:alert] = "投稿できませんでした"
+      render :new
+    end
   end
 
   def index
@@ -22,4 +34,14 @@ class Public::FeelingPostsController < ApplicationController
 
   def search
   end
+
+  private
+
+  def feeling_post_params
+    params.require(:feeling_post).permit(:title, :body)
+    # params：formから送られてくるデータが入ってる
+    # require：送られてきたデータの中からモデル名(ここでは:feeling_post)を指定しデータを絞り込む
+    # permit：requireで絞り込んだデータの中から保存を許可するカラムを指定
+  end
+
 end
